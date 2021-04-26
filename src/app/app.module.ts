12d69e80cdb6c7e240d68c52e6b10deefb5737b2
lucasdiniz063 <lucasd.almeida0@gmail.com>
2021-04-26 03:51:08 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/Forms';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { starComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    starComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
